3cf2f5bc25cf03d004154ee62eba02133dcd27f3
Ole Lynge Soerensen <olelynge@gmail.com>
2015-04-11 16:43:58 +0200

    Method 2: Render view to string in web api.

---
+++ RazorFromApiSpike.Api/Controllers/ApiRenderRazorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Http;
+using RazorFromApiSpike.Api.Models;
+
+namespace RazorFromApiSpike.Api.Controllers
+{
+    public class ApiRazorResult
+    {
+        public ApiRazorResult(string html)
+        {
+            Html = html;
+        }
+
+        public string Html { get; private set; }
+    }
+
+    public class ApiRenderRazorController : ApiController
+    {
+        // GET api/apirenderrazor/5
+        public ApiRazorResult Get(int id)
+        {
+            return new ApiRazorResult(RenderHtml());
+        }
+
+        private static string RenderHtml()
+        {
+            return ViewRenderer.RenderView(
+                "~/Views/ApiRenderRazor/Index.cshtml",
+                DateTime.Now,
+                true);
+        }
+    }
+}
---
+++ RazorFromApiSpike.Api/Models/ViewRenderer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RazorFromApiSpike.Api.Models
+{
+    public static class ViewRenderer
+    {
+        public static string RenderView(
+            string viewPath,
+            object model = null,
+            bool partial = false)
+        {
+            return RenderView(
+                CreateController<GenericController>().ControllerContext,
+                viewPath,
+                model,
+                partial);
+        }
+
+        private static string RenderView(
+            ControllerContext context,
+            string viewPath,
+            object model = null,
+            bool partial = false)
+        {
+            // first find the ViewEngine for this view
+            ViewEngineResult viewEngineResult = null;
+            if (partial)
+                viewEngineResult = ViewEngines.Engines.FindPartialView(context, viewPath);
+            else
+                viewEngineResult = ViewEngines.Engines.FindView(context, viewPath, null);
+
+            if (viewEngineResult == null)
+                throw new FileNotFoundException("View cannot be found.");
+
+            // get the view and attach the model to view data
+            var view = viewEngineResult.View;
+            context.Controller.ViewData.Model = model;
+
+            string result = null;
+
+            using (var sw = new StringWriter())
+            {
+                var ctx = new ViewContext(context, view,
+                    context.Controller.ViewData,
+                    context.Controller.TempData,
+                    sw);
+                view.Render(ctx, sw);
+                result = sw.ToString();
+            }
+
+            return result;
+        }
+
+        private static T CreateController<T>(RouteData routeData = null)
+            where T : Controller, new()
+        {
+            // create a disconnected controller instance
+            var controller = new T();
+
+            // get context wrapper from HttpContext if available
+            HttpContextBase wrapper;
+            if (HttpContext.Current != null)
+                wrapper = new HttpContextWrapper(HttpContext.Current);
+            else
+                throw new InvalidOperationException(
+                    "Can't create Controller Context if no " +
+                    "active HttpContext instance is available.");
+
+            if (routeData == null)
+                routeData = new RouteData();
+
+            // add the controller routing if not existing
+            if (!routeData.Values.ContainsKey("controller") &&
+                !routeData.Values.ContainsKey("Controller"))
+                routeData.Values.Add("controller",
+                    controller.GetType()
+                        .Name.ToLower().Replace("controller", ""));
+
+            controller.ControllerContext = new ControllerContext(wrapper, routeData, controller);
+            return controller;
+        }
+
+        private class GenericController : Controller
+        {
+        }
+    }
+}
